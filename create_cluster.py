db27c3107448099235adf76fd46b558d7c4fe55e
fabiog <ghirardello.fabio@gmail.com>
2019-04-03 16:48:09 -0400

    Update create_cluster.py

--- create_cluster.py
+++ create_cluster.py
@@ -15,14 +15,20 @@ cm_api = cm_client.ClouderaManagerResourceApi(api_client)
 # accept trial licence
 cm_api.begin_trial()
 
-# create hosts
+# create hosts TODO: delete if not needed
 #hosts_api = cm_client.HostsResourceApi(api_client)
 #api_host_list = cm_client.ApiHostList([cm_client.ApiHost(hostname='YourHostName', ip_address='YourHostIpAddress')])
 #hosts_api.create_hosts(body=api_host_list)
 
+# Install CM Agent on host
+
+with open ("/root/myRSAkey", "r") as f:
+    key = f.read()
+
+
 instargs = cm_client.ApiHostInstallArguments(host_names=['fab.ue2v4pwgnmtevd3wmxx0ndpkka.bx.internal.cloudapp.net'], 
                                              user_name='root', 
-                                             private_key=YourPrivateKey, 
+                                             private_key=key, 
                                              cm_repo_url='https://archive.cloudera.com/cm6/6.2.0', 
                                              java_install_strategy='NONE', 
                                              ssh_port=22, 
@@ -45,8 +51,8 @@ mgmt_api.setup_cms(body=api_service)
 mgmt_api.start_command()
 
 # create the cluster using the template
-with open('OneNodeCluster_template.json') as in_file:
-    json_str = in_file.read()
+with open('OneNodeCluster_template.json') as f:
+    json_str = f.read()
 
 Response = namedtuple("Response", "data")
 dst_cluster_template=api_client.deserialize(response=Response(json_str),response_type=cm_client.ApiClusterTemplate)
