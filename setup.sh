a800eafd8a7c2e799aadc4f066b5e28d1c074024
fabiog <ghirardello.fabio@gmail.com>
2019-04-17 10:52:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
+#! /bin/bash
+
+# Check input parameters
 if [ -z $1 ]
 then
     echo "must add parameter ['aws' | 'azure' | 'gcp']"
     echo "example: ./setup.sh aws"
     exit 1
 fi
+
+if [ -z $2 ]
+then
+    TEMPLATE="default_template.json"
+else
+    TEMPLATE=$2
+fi
+echo "------------------------------------------------------"
+echo "Template is [" $TEMPLATE "]"
 
 echo "------------------------------------------------------"
 echo "-- Configure the OS"
@@ -40,7 +52,7 @@ echo "-- Install CM and MariaDB"
 wget https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
 rpm --import https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/RPM-GPG-KEY-cloudera
 yum install -y cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server mariadb-server
-cat mariadb.config > /etc/my.cnf 
+cat mariadb.config > /etc/my.cnf
 
 
 echo "------------------------------------------------------"
@@ -49,7 +61,7 @@ wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /
 wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
 wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
 
-chown cloudera-scm:cloudera-scm /opt/cloudera/csd/* 
+chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
 chmod 644 /opt/cloudera/csd/*
 
 
@@ -60,10 +72,10 @@ systemctl start mariadb
 
 echo "------------------------------------------------------"
 echo "-- Install JDBC connector"
-wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz
-tar zxf mysql-connector-java-5.1.46.tar.gz
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~
+tar zxf ~/mysql-connector-java-5.1.46.tar.gz -C ~
 mkdir -p /usr/share/java/
-cp mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
+cp ~/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
 
 echo "------------------------------------------------------"
 echo "-- Create DBs required by CM"
@@ -82,7 +94,7 @@ echo "-- Enable passwordless root login via rsa key"
 ssh-keygen -f ~/myRSAkey -t rsa -N ""
 mkdir ~/.ssh
 cat ~/myRSAkey.pub >> ~/.ssh/authorized_keys
-chmod 400 ~/.ssh/authorized_keys 
+chmod 400 ~/.ssh/authorized_keys
 ssh-keyscan -H `hostname` >> ~/.ssh/known_hosts
 sed -i 's/.*PermitRootLogin.*/PermitRootLogin without-password/' /etc/ssh/sshd_config
 systemctl restart sshd
@@ -94,8 +106,8 @@ systemctl start cloudera-scm-server
 
 while [ `curl -s -X GET -u "admin:admin"  http://localhost:7180/api/version` -z ] ;
     do
-    echo "waiting 10s for CM to come up.."; 
-    sleep 10; 
+    echo "waiting 10s for CM to come up..";
+    sleep 10;
 done
 
 
@@ -107,10 +119,8 @@ yum install -y python-pip
 pip install --upgrade pip
 pip install cm_client
 
-sed -i "s/YourHostName/`hostname`/g" ~/OneNodeCDHCluster/default_template.json
+sed -i "s/YourHostName/`hostname`/g" ~/OneNodeCDHCluster/$TEMPLATE
 sed -i "s/YourHostName/`hostname`/g" ~/OneNodeCDHCluster/create_cluster.py
-python ~/OneNodeCDHCluster/create_cluster.py
+python ~/OneNodeCDHCluster/create_cluster.py $TEMPLATE
 
 echo "-- At this point you can login into Cloudera Manager host on port 7180 and follow the deployment of the cluster"
-
-
