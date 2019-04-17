bb4dc9e36a5906da7671ade9ee6031c07e2e25e7
fabiog <ghirardello.fabio@gmail.com>
2019-04-17 10:53:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,126 +1,92 @@
-#! /bin/bash
+from __future__ import print_function
+import cm_client
+from cm_client.rest import ApiException
+from collections import namedtuple
+from pprint import pprint
+import json
+import time
+import sys
 
-# Check input parameters
-if [ -z $1 ]
-then
-    echo "must add parameter ['aws' | 'azure' | 'gcp']"
-    echo "example: ./setup.sh aws"
-    exit 1
-fi
+def wait(cmd, timeout=None):
+    SYNCHRONOUS_COMMAND_ID = -1
+    if cmd.id == SYNCHRONOUS_COMMAND_ID:
+        return cmd
 
-if [ -z $2 ]
-then
-    TEMPLATE="default_template.json"
-else
-    TEMPLATE=$2
-fi
-echo "------------------------------------------------------"
-echo "Template is [" $TEMPLATE "]"
+    SLEEP_SECS = 5
+    if timeout is None:
+        deadline = None
+    else:
+        deadline = time.time() + timeout
 
-echo "------------------------------------------------------"
-echo "-- Configure the OS"
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-# TODO make 2 above commands to be permanent
-echo  "vm.swappiness = 1" >> /etc/sysctl.conf
-sysctl vm.swappiness=1
+    try:
+        cmd_api_instance = cm_client.CommandsResourceApi(api_client)
+        while True:
+            cmd = cmd_api_instance.read_command(long(cmd.id))
+            pprint(cmd)
+            if not cmd.active:
+                return cmd
 
-echo "------------------------------------------------------"
-echo "-- Install Java OpenJDK8 and other tools, and run 'os-generic-bootstrap.sh'"
-yum install -y java-1.8.0-openjdk-devel vim wget curl git
-
-if [ $1 = "azure" ]
-then
-    echo "It's azure!"
-    curl -sSL https://raw.githubusercontent.com/cloudera/director-scripts/master/azure-bootstrap-scripts/os-generic-bootstrap.sh | sh
-    sleep 10
-fi
-
-echo "------------------------------------------------------"
-echo "-- Configure networking"
-hostnamectl set-hostname `hostname -f`
-echo "`hostname -I` `hostname`" >> /etc/hosts
-sed -i "s/HOSTNAME=.*/HOSTNAME=`hostname`/" /etc/sysconfig/network
-iptables-save > ~/firewall.rules
-systemctl disable firewalld
-systemctl stop firewalld
-setenforce 0
-sed -i 's/SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
-
-echo "------------------------------------------------------"
-echo "-- Install CM and MariaDB"
-wget https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
-rpm --import https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/RPM-GPG-KEY-cloudera
-yum install -y cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server mariadb-server
-cat mariadb.config > /etc/my.cnf
+            if deadline is not None:
+                now = time.time()
+                if deadline < now:
+                    return cmd
+                else:
+                    time.sleep(min(SLEEP_SECS, deadline - now))
+            else:
+                time.sleep(SLEEP_SECS)
+    except ApiException as e:
+        print("Exception when calling ClouderaManagerResourceApi->import_cluster_template: %s\n" % e)
 
 
-echo "------------------------------------------------------"
-echo "-- Install CSDs"
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+cm_client.configuration.username = 'admin'
+cm_client.configuration.password = 'admin'
+api_client = cm_client.ApiClient("http://localhost:7180/api/v32")
 
-chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
-chmod 644 /opt/cloudera/csd/*
+cm_api = cm_client.ClouderaManagerResourceApi(api_client)
 
+# accept trial licence
+cm_api.begin_trial()
 
-echo "------------------------------------------------------"
-echo "--Enable and start MariaDB"
-systemctl enable mariadb
-systemctl start mariadb
-
-echo "------------------------------------------------------"
-echo "-- Install JDBC connector"
-wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~
-tar zxf ~/mysql-connector-java-5.1.46.tar.gz -C ~
-mkdir -p /usr/share/java/
-cp ~/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
-
-echo "------------------------------------------------------"
-echo "-- Create DBs required by CM"
-mysql -u root < ~/OneNodeCDHCluster/create_db.sql
-
-echo "------------------------------------------------------"
-echo "-- Secure MariaDB"
-mysql -u root < ~/OneNodeCDHCluster/secure_mariadb.sql
-
-echo "------------------------------------------------------"
-echo "-- Prepare CM database 'scm'"
-/opt/cloudera/cm/schema/scm_prepare_database.sh mysql scm scm cloudera
-
-echo "------------------------------------------------------"
-echo "-- Enable passwordless root login via rsa key"
-ssh-keygen -f ~/myRSAkey -t rsa -N ""
-mkdir ~/.ssh
-cat ~/myRSAkey.pub >> ~/.ssh/authorized_keys
-chmod 400 ~/.ssh/authorized_keys
-ssh-keyscan -H `hostname` >> ~/.ssh/known_hosts
-sed -i 's/.*PermitRootLogin.*/PermitRootLogin without-password/' /etc/ssh/sshd_config
-systemctl restart sshd
+# Install CM Agent on host
+with open ("/root/myRSAkey", "r") as f:
+    key = f.read()
 
 
-echo "------------------------------------------------------"
-echo "-- Start CM, it takes about 2 minutes to be ready"
-systemctl start cloudera-scm-server
+instargs = cm_client.ApiHostInstallArguments(host_names=['ip-10-0-1-88.ec2.internal'],
+                                             user_name='root',
+                                             private_key=key,
+                                             cm_repo_url='https://archive.cloudera.com/cm6/6.2.0',
+                                             java_install_strategy='NONE',
+                                             ssh_port=22,
+                                             passphrase='')
 
-while [ `curl -s -X GET -u "admin:admin"  http://localhost:7180/api/version` -z ] ;
-    do
-    echo "waiting 10s for CM to come up..";
-    sleep 10;
-done
+cmd = cm_api.host_install_command(body=instargs)
+wait(cmd)
 
+# create MGMT/CMS
+mgmt_api = cm_client.MgmtServiceResourceApi(api_client)
+api_service = cm_client.ApiService()
 
-echo "------------------------------------------------------"
-echo "-- Now CM is started and the next step is to automate using the CM API"
+api_service.roles = [cm_client.ApiRole(type='SERVICEMONITOR'),
+    cm_client.ApiRole(type='HOSTMONITOR'),
+    cm_client.ApiRole(type='EVENTSERVER'),
+    cm_client.ApiRole(type='ALERTPUBLISHER')]
 
-yum install -y epel-release
-yum install -y python-pip
-pip install --upgrade pip
-pip install cm_client
+mgmt_api.auto_assign_roles() # needed?
+mgmt_api.auto_configure()    # needed?
+mgmt_api.setup_cms(body=api_service)
+cmd = mgmt_api.start_command()
+wait(cmd)
 
-sed -i "s/YourHostName/`hostname`/g" ~/OneNodeCDHCluster/$TEMPLATE
-sed -i "s/YourHostName/`hostname`/g" ~/OneNodeCDHCluster/create_cluster.py
-python ~/OneNodeCDHCluster/create_cluster.py $TEMPLATE
+# create the cluster using the template
+with open(sys.argv[1]) as f:
+    json_str = f.read()
 
-echo "-- At this point you can login into Cloudera Manager host on port 7180 and follow the deployment of the cluster"
+Response = namedtuple("Response", "data")
+dst_cluster_template=api_client.deserialize(response=Response(json_str),response_type=cm_client.ApiClusterTemplate)
+cmd = cm_api.import_cluster_template(add_repositories=True, body=dst_cluster_template)
+wait(cmd)
+
+# API Docs for reference
+# https://archive.cloudera.com/cm6/6.2.0/generic/jar/cm_api/swagger-html-sdk-docs/python/docs/ClouderaManagerResourceApi.html
+# https://archive.cloudera.com/cm6/6.2.0/generic/jar/cm_api/swagger-html-sdk-docs/python/docs/MgmtServiceResourceApi.html
