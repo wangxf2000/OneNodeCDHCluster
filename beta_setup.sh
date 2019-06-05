4fc79dae85c418d2ff8a711a40466ea1c3a286d5
fabiog <ghirardello.fabio@gmail.com>
2019-06-05 14:10:39 -0400

    Create beta_setup.sh

---
+++ beta_setup.sh
@@ -0,0 +1,238 @@
+#! /bin/bash
+networkmanager_7()
+{
+cat > /etc/NetworkManager/dispatcher.d/12-register-dns <<"EOF"
+#!/bin/bash
+# NetworkManager Dispatch script
+# Deployed by Cloudera Altus Director Bootstrap
+#
+# Expected arguments:
+#    $1 - interface
+#    $2 - action
+#
+# See for info: http://linux.die.net/man/8/networkmanager
+# Register A and PTR records when interface comes up
+# only execute on the primary nic
+if [ "$1" != "eth0" ] || [ "$2" != "up" ]
+then
+    exit 0;
+fi
+# when we have a new IP, perform nsupdate
+new_ip_address="$DHCP4_IP_ADDRESS"
+host=$(hostname -s)
+domain=$(nslookup $(grep -i nameserver /etc/resolv.conf | cut -d ' ' -f 2) | grep -i name | cut -d ' ' -f 3 | cut -d '.' -f 2- | rev | cut -c 2- | rev)
+IFS='.' read -ra ipparts <<< "$new_ip_address"
+ptrrec="$(printf %s "$new_ip_address." | tac -s.)in-addr.arpa"
+nsupdatecmds=$(mktemp -t nsupdate.XXXXXXXXXX)
+resolvconfupdate=$(mktemp -t resolvconfupdate.XXXXXXXXXX)
+echo updating resolv.conf
+grep -iv "search" /etc/resolv.conf > "$resolvconfupdate"
+echo "search $domain" >> "$resolvconfupdate"
+cat "$resolvconfupdate" > /etc/resolv.conf
+echo "Attempting to register $host.$domain and $ptrrec"
+{
+    echo "update delete $host.$domain a"
+    echo "update add $host.$domain 600 a $new_ip_address"
+    echo "send"
+    echo "update delete $ptrrec ptr"
+    echo "update add $ptrrec 600 ptr $host.$domain"
+    echo "send"
+} > "$nsupdatecmds"
+nsupdate "$nsupdatecmds"
+exit 0;
+EOF
+chmod 755 /etc/NetworkManager/dispatcher.d/12-register-dns
+service network restart
+
+# Confirm DNS record has been updated, retry if update did not work
+i=0
+until [ $i -ge 5 ]
+do
+    sleep 5
+    i=$((i+1))
+    hostname | nslookup && break
+    service network restart
+done
+
+if [ $i -ge 5 ]; then
+    echo "DNS update failed"
+    exit 1
+fi
+}
+
+
+################
+# MAIN PROGRAM # 
+################
+echo "-- Configure and optimize the OS"
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.d/rc.local
+echo "echo never > /sys/kernel/mm/transparent_hugepage/defrag" >> /etc/rc.d/rc.local
+# add tuned optimization https://www.cloudera.com/documentation/enterprise/6/6.2/topics/cdh_admin_performance.html
+echo  "vm.swappiness = 1" >> /etc/sysctl.conf
+sysctl vm.swappiness=1
+timedatectl set-timezone UTC
+# CDSW requires Centos 7.5, so we trick it to believe it is...
+echo "CentOS Linux release 7.5.1810 (Core)" > /etc/redhat-release
+
+echo "-- Install Java OpenJDK8 and other tools"
+yum install -y java-1.8.0-openjdk-devel vim wget curl git bind-utils
+
+# Check input parameters
+case "$1" in
+        aws)
+            echo "server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4" >> /etc/chrony.conf
+            systemctl restart chronyd
+            ;;
+        azure)
+            networkmanager_7
+            sleep 10
+            umount /mnt/resource
+            mount /dev/sdb1 /opt
+            ;;
+        gcp)
+            ;;
+        openstack)
+            echo "Not supported yet!"
+            exit 1
+            ;;
+        *)
+            echo $"Usage: $0 {aws|azure|gcp} template-file [docker-device]"
+            echo $"example: ./setup.sh azure default_template.json"
+            echo $"example: ./setup.sh aws cdsw_template.json /dev/xvdb"
+            exit 1
+esac
+
+TEMPLATE=$2
+# ugly, but for now the docker device has to be put by the user
+DOCKERDEVICE=$3
+
+
+echo "-- Configure networking"
+PUBLIC_IP=`curl https://api.ipify.org/`
+hostnamectl set-hostname `hostname -f`
+echo "`hostname -I` `hostname`" >> /etc/hosts
+sed -i "s/HOSTNAME=.*/HOSTNAME=`hostname`/" /etc/sysconfig/network
+iptables-save > ~/firewall.rules
+systemctl disable firewalld
+systemctl stop firewalld
+setenforce 0
+sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+echo "-- Install CM"
+wget https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
+yum install -y cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server
+
+## MySQL
+#rpm --import https://archive.cloudera.com/cm6/6.2.0/redhat7/yum/RPM-GPG-KEY-cloudera
+#wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+#yes | rpm -ivh mysql-community-release-el7-5.noarch.rpm
+#yum install -y mysql-server
+#cat mysql.config > /etc/my.cnf
+
+## MariaDB 10.1
+cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+EOF
+
+yum install -y MariaDB-server MariaDB-client
+cat mariadb.config > /etc/my.cnf
+
+
+echo "--Enable and start MariaDB"
+systemctl enable mariadb
+systemctl start mariadb
+
+echo "-- Install JDBC connector"
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~
+tar zxf ~/mysql-connector-java-5.1.46.tar.gz -C ~
+mkdir -p /usr/share/java/
+cp ~/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
+
+echo "-- Create DBs required by CM"
+mysql -u root < ~/OneNodeCDHCluster/create_db.sql
+
+echo "-- Secure MariaDB"
+mysql -u root < ~/OneNodeCDHCluster/secure_mariadb.sql
+
+echo "-- Prepare CM database 'scm'"
+/opt/cloudera/cm/schema/scm_prepare_database.sh mysql scm scm cloudera
+
+echo "-- Install CSDs"
+wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/cdsw1/1.5.0/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.5.0.jar -P /opt/cloudera/csd/
+# CSD for C5
+wget https://archive.cloudera.com/cdsw1/1.5.0/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH5-1.5.0.jar -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/spark2/csd/SPARK2_ON_YARN-2.4.0.cloudera1.jar -P /opt/cloudera/csd/
+chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
+chmod 644 /opt/cloudera/csd/*
+
+echo "-- Install CEM Tarballs"
+mkdir -p /opt/cloudera/cem
+wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
+ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
+ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
+ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
+chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
+rm -f /opt/cloudera/cem/efm/conf/efm.properties
+cp ~/OneNodeCDHCluster/efm.properties /opt/cloudera/cem/efm/conf
+rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
+cp ~/OneNodeCDHCluster/bootstrap.conf /opt/cloudera/cem/minifi/conf
+sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
+sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
+/opt/cloudera/cem/minifi/bin/minifi.sh install
+
+
+echo "-- Enable passwordless root login via rsa key"
+ssh-keygen -f ~/myRSAkey -t rsa -N ""
+mkdir ~/.ssh
+cat ~/myRSAkey.pub >> ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/authorized_keys
+ssh-keyscan -H `hostname` >> ~/.ssh/known_hosts
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin without-password/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+echo "-- Start CM, it takes about 2 minutes to be ready"
+systemctl start cloudera-scm-server
+
+while [ `curl -s -X GET -u "admin:admin"  http://localhost:7180/api/version` -z ] ;
+    do
+    echo "waiting 10s for CM to come up..";
+    sleep 10;
+done
+
+echo "-- Now CM is started and the next step is to automate using the CM API"
+
+yum install -y epel-release
+yum install -y python-pip
+pip install --upgrade pip
+pip install cm_client
+
+sed -i "s/YourHostname/`hostname -f`/g" ~/OneNodeCDHCluster/$TEMPLATE
+sed -i "s/YourCDSWDomain/cdsw.$PUBLIC_IP.nip.io/g" ~/OneNodeCDHCluster/$TEMPLATE
+sed -i "s/YourPrivateIP/`hostname -I | tr -d '[:space:]'`/g" ~/OneNodeCDHCluster/$TEMPLATE
+sed -i "s#YourDockerDevice#$DOCKERDEVICE#g" ~/OneNodeCDHCluster/$TEMPLATE
+
+sed -i "s/YourHostname/`hostname -f`/g" ~/OneNodeCDHCluster/create_cluster.py
+
+python ~/OneNodeCDHCluster/create_cluster.py $TEMPLATE
+
+# configure and start EFM and Minifi
+service efm start
+service minifi start
+
+echo "-- At this point you can login into Cloudera Manager host on port 7180 and follow the deployment of the cluster"
