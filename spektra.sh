e967673a2a61c372bdb30995f065d86c37a925a8
fabiog <ghirardello.fabio@gmail.com>
2019-08-07 11:21:56 -0400

    Update spektra.sh

--- spektra.sh
+++ spektra.sh
@@ -55,12 +55,19 @@ echo "-- Prepare CM database 'scm'"
 /opt/cloudera/cm/schema/scm_prepare_database.sh mysql scm scm cloudera
 
 echo "-- Install CSDs"
+# install local CSDs
+mv ~/*.jar /opt/cloudera/csd/
+
 wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
 wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
 wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
 wget https://archive.cloudera.com/cdsw1/1.6.0/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.0.jar -P /opt/cloudera/csd/
 chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
 chmod 644 /opt/cloudera/csd/*
+
+echo "-- Install local parcels"
+mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
+chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
 
 echo "-- Install CEM Tarballs"
 mkdir -p /opt/cloudera/cem
