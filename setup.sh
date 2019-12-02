96cdf48586afa197e86dc54e22160a79b3f7ce85
wangxf2000 <wangxf.2000@gmail.com>
2019-12-02 11:18:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -179,6 +179,11 @@ pip install paho-mqtt
 systemctl enable mosquitto
 systemctl start mosquitto
 
+echo "--Download the NiFi MQTT Processor to read from mosquitto"
+wget http://central.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
+chown root:root /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
+chmod 660 /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
+
 echo "--Now start efm"
 # configure and start EFM and Minifi
 service efm start
