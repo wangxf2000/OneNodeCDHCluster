edfb8d3f11980faad6f3ef31f284c0a02e6b0eb0
wangxf2000 <wangxf.2000@gmail.com>
2019-12-06 11:20:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -172,21 +172,5 @@ sed -i "s/YourHostname/`hostname -f`/g" ~/OneNodeCDHCluster/scripts/create_clust
 
 python ~/OneNodeCDHCluster/scripts/create_cluster.py $TEMPLATE
 
-echo "--Now install required libs and start the mosquitto broker"
-
-yum install -y mosquitto
-pip install paho-mqtt
-systemctl enable mosquitto
-systemctl start mosquitto
-
-echo "--Download the NiFi MQTT Processor to read from mosquitto"
-wget http://central.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
-chown root:root /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
-chmod 660 /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
-
-echo "--Now start efm"
-# configure and start EFM and Minifi
-service efm start
-service minifi start
 
 echo "-- At this point you can login into Cloudera Manager host on port 7180 and follow the deployment of the cluster"
