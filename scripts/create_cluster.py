e7975b0fd935ae5deb818af2cfeb954489e20754
wangxf2000 <wangxf.2000@gmail.com>
2021-03-11 15:03:22 +0800

    Update create_cluster.py
    update to cm 6.3.4

--- scripts/create_cluster.py
+++ scripts/create_cluster.py
@@ -55,7 +55,7 @@ with open ("/root/myRSAkey", "r") as f:
 instargs = cm_client.ApiHostInstallArguments(host_names=['YourHostname'], 
                                              user_name='root', 
                                              private_key=key, 
-                                             cm_repo_url='http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.16.2/', 
+                                             cm_repo_url='http://archive.cloudera.com/cm6/redhat/7/x86_64/cm/6.3.4/', 
                                              java_install_strategy='NONE', 
                                              ssh_port=22, 
                                              passphrase='')
