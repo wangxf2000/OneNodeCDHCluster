f978b15e9ccf74bcda8571d7eac627840a2163cc
wangxf2000 <wangxf.2000@gmail.com>
2021-03-11 15:02:10 +0800

    Update setup.sh
    update to cm6.3.4,cdsw 1.9.1

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
 
 
 echo "-- Install CM and MariaDB repo"
-wget https://archive.cloudera.com/cm6/6.3.1/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
+wget https://archive.cloudera.com/cm6/6.3.4/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
 
 ## MariaDB 10.1
 cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
@@ -100,13 +100,13 @@ echo "-- Install CSDs"
 # install local CSDs
 mv ~/*.jar /opt/cloudera/csd/
 
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/cdsw1/1.6.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar -P /opt/cloudera/csd/
+#wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+#wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+#wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/cdsw1/1.6.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.9.1.jar -P /opt/cloudera/csd/
 # CSD for C5
-wget https://archive.cloudera.com/cdsw1/1.6.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH5-1.6.1.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/spark2/csd/SPARK2_ON_YARN-2.4.0.cloudera1.jar -P /opt/cloudera/csd/
+#wget https://archive.cloudera.com/cdsw1/1.6.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH5-1.6.1.jar -P /opt/cloudera/csd/
+#wget https://archive.cloudera.com/spark2/csd/SPARK2_ON_YARN-2.4.0.cloudera1.jar -P /opt/cloudera/csd/
 
 chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
 chmod 644 /opt/cloudera/csd/*
@@ -115,40 +115,40 @@ echo "-- Install local parcels"
 mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
 chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
 
-echo "-- Install CEM Tarballs"
-mkdir -p /opt/cloudera/cem
-wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
-ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
-ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
-ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
-chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
-rm -f /opt/cloudera/cem/efm/conf/efm.properties
-cp ~/OneNodeCDHCluster/conf/efm.properties /opt/cloudera/cem/efm/conf
-rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
-cp ~/OneNodeCDHCluster/conf/bootstrap.conf /opt/cloudera/cem/minifi/conf
-sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
-sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
-/opt/cloudera/cem/minifi/bin/minifi.sh install
+#echo "-- Install CEM Tarballs"
+#mkdir -p /opt/cloudera/cem
+#wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
+#tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
+#tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+#tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+#tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+#rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
+#ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
+#ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
+#ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
+#chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
+#chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
+#chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
+#rm -f /opt/cloudera/cem/efm/conf/efm.properties
+#cp ~/OneNodeCDHCluster/conf/efm.properties /opt/cloudera/cem/efm/conf
+#rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
+#cp ~/OneNodeCDHCluster/conf/bootstrap.conf /opt/cloudera/cem/minifi/conf
+#sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
+#sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
+#/opt/cloudera/cem/minifi/bin/minifi.sh install
 
-echo "--Now install required libs and start the mosquitto broker"
+#echo "--Now install required libs and start the mosquitto broker"
 
-yum install -y mosquitto
-pip install paho-mqtt
-systemctl enable mosquitto
-systemctl start mosquitto
+#yum install -y mosquitto
+#pip install paho-mqtt
+#systemctl enable mosquitto
+#systemctl start mosquitto
 
-echo "--Download the NiFi MQTT Processor to read from mosquitto"
-mkdir -p /opt/cloudera/cem/minifi/lib/
-wget https://repo1.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
-chown root:root /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
-chmod 660 /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
+#echo "--Download the NiFi MQTT Processor to read from mosquitto"
+#mkdir -p /opt/cloudera/cem/minifi/lib/
+#wget https://repo1.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
+#chown root:root /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
+#chmod 660 /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
 
 echo "-- Enable passwordless root login via rsa key"
 ssh-keygen -f ~/myRSAkey -t rsa -N ""
