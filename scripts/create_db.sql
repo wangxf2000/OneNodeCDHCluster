58205ce86804a68fbbfb4060310e066766be9149
wangxf2000 <wangxf.2000@gmail.com>
2025-01-17 08:30:02 +0800

    Update create_db.sql
    update password

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,38 +1,38 @@
 CREATE DATABASE scm DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON scm.* TO 'scm'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON scm.* TO 'scm'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE amon DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON amon.* TO 'amon'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON amon.* TO 'amon'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE rman DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON rman.* TO 'rman'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON rman.* TO 'rman'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE hue DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON hue.* TO 'hue'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON hue.* TO 'hue'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE metastore DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON metastore.* TO 'hive'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON metastore.* TO 'hive'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE sentry DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON sentry.* TO 'sentry'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON sentry.* TO 'sentry'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE nav DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON nav.* TO 'nav'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON nav.* TO 'nav'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE navms DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON navms.* TO 'navms'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON navms.* TO 'navms'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE oozie DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON oozie.* TO 'oozie'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON oozie.* TO 'oozie'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE efm DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON efm.* TO 'efm'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON efm.* TO 'efm'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE nifireg DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON nifireg.* TO 'nifireg'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON nifireg.* TO 'nifireg'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE registry DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON registry.* TO 'registry'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON registry.* TO 'registry'@'%' IDENTIFIED BY 'Gyzq!123';
 
 CREATE DATABASE streamsmsgmgr DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON streamsmsgmgr.* TO 'streamsmsgmgr'@'%' IDENTIFIED BY 'cloudera';
+GRANT ALL ON streamsmsgmgr.* TO 'streamsmsgmgr'@'%' IDENTIFIED BY 'Gyzq!123';
